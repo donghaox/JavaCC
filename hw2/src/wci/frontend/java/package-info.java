68b57b7f57fa08cf2ad820a6984e5f6cc69fe0ea
haiMac <mvamtv@gmail.com>
2015-09-09 00:31:11 -0700

    add new package with package infor

---
+++ hw2/src/wci/frontend/java/package-info.java
@@ -0,0 +1,9 @@
+/**
+ *  This is a new front-end package for the (simplified) Java programming
+ *   language used for language-independent framework
+ */
+/**
+ * @author hai
+ *
+ */
+package wci.frontend.java;
