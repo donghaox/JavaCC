c8cd38099970f3cdc3deb60245fc03394528b2eb
haiMac <mvamtv@gmail.com>
2015-11-28 16:35:29 -0800

    grammar first commit.

--- hw2/src/wci/intermediate/Definition.java
+++
@@ -1,8 +0,0 @@
-package wci.intermediate;
-
-/**
- * Created by Haoxuan Dong on 10/9/2015.
- */
-public interface Definition {
-    public String getText();
-}
--- hw2/src/wci/intermediate/TypeKey.java
+++
@@ -1,7 +0,0 @@
-package wci.intermediate;
-
-/**
- * Created by Haoxuan Dong on 10/9/2015.
- */
-public interface TypeKey {
-}
